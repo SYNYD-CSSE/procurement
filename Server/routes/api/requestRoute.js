eb095b98639b50395017ca5d86838d101afec999
Supun Dileepa <dilee1st.1995@gmail.com>
2018-09-13 20:51:38 +0530

    requestRoute added

--- Server/routes/api/requestRoute.js
+++ Server/routes/api/requestRoute.js
@@ -1,49 +1,70 @@
 const express = require("express");
 const router = express.Router();
 const Request = require("../../models/request");
+const Item = require("../../models/item");
 
-router.post("/order", (req, res, next) => {
-  try {
-    const request = new Request({
-      orderId: req.body.orderId,
-      itemList: [
-        {
-          quantity: req.body.quantity,
-          name: req.body.name
-        }
-      ],
 
-      status: req.body.status
-    });
-    request.save((err, result) => {
-      if (err) {
-        return res.status(500).json({
-          title: "An error occured",
-          error: err
+
+router.post("/add/item", (req, res, next) => {
+    try {
+        const item = new Item({
+            name: req.body.name,
+            quantity: req.body.quantity,
         });
-      }
-      res.status(201).json({
-        message: "Order Placed",
-        obj: result
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  }
+        item.save((err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    title: "An error occured",
+                    error: err
+                });
+            }
+            res.status(201).json({
+                message: "Item Added",
+                result
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-// router.get(
-//     "/current",
-//     passport.authenticate("jwt", {
-//         session: false
-//     }),
-//     (req, res) => {
-//         res.json({
-//             id: req.user._id,
-//             firstName: req.user.firstName,
-//             email: req.user.email
-//         });
-//     }
-// );
+
+
+router.post("/order", (req, res, next) => {
+    try {
+        const request = new Request({
+            orderId: req.body.orderId,
+            itemList: request._id,
+            status: req.body.status
+        });
+        request.save((err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    title: "An error occured",
+                    error: err
+                });
+            }
+            res.status(201).json({
+                message: "Order Placed",
+                obj: result
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/order", (req, res, next) => {
+    try {
+
+        Request.find(function (err, requset) {
+            if (err) return next(err);
+            res.json(requset);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
