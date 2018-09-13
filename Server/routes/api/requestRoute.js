113c7621110983c872a4ed6da5c237782a85591e
Supun Dileepa <dilee1st.1995@gmail.com>
2018-09-13 14:00:04 +0530

    test

--- Server/routes/api/requestRoute.js
+++ Server/routes/api/requestRoute.js
@@ -24,62 +24,7 @@ router.post("/order", (req, res, next) => {
     });
 });
 
-router.post("/login", (req, res, next) => {
-    const {
-        errors,
-        isValid
-    } = validateLoginInput(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-        if (err) {
-            return res.status(500).json({
-                title: "An error occured",
-                error: err
-            });
-        }
-        if (!user) {
-            return res.status(401).json({
-                title: "Login Failed",
-                error: {
-                    message: "Invalid login credentials"
-                }
-            });
-        }
-
-        if (!bcrypt.compareSync(req.body.password, user.password)) {
-            return res.status(401).json({
-                title: "Login Failed",
-                error: {
-                    message: "Invalid login credentials"
-                }
-            });
-        }
-
-        const token = jwt.sign({
-            user: user
-        }, keys.secretOrKey, {
-            expiresIn: 3600
-        });
-        if (err) {
-            return res.status(500).json({
-                title: "An error occured",
-                error: err
-            });
-        } else {
-            res.status(200).json({
-                message: "Successfully Logged In",
-                token: "Bearer " + token,
-                userId: user._id
-            });
-        }
-    });
-});
 
 router.get(
     "/current",
