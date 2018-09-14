365995e4679409a6761dc9e28a2f1f2d9b96ef03
Samith Dilshan <samithdilsh@gmail.com>
2018-09-14 14:27:11 +0530

    TypeScript Build Initialized

---
+++ Server/build/models/item.js
@@ -0,0 +1,15 @@
+"use strict";
+const mongoose = require("mongoose");
+const mongooseUniqueValidator = require("mongoose-unique-validator");
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+module.exports = mongoose.model("Item", itemSchema);
+//# sourceMappingURL=item.js.map
---
+++ Server/build/server.js
@@ -0,0 +1,36 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+//const passport = require("passport");
+const config = require("./config/database");
+const Request = require("./routes/api/requestRoute");
+mongoose.connect(config.database);
+mongoose.connection.on("connected", () => {
+    console.log(`connected to database ${config.database}`);
+});
+mongoose.connection.on("error", err => {
+    console.log(`database connection failed ${err}`);
+});
+const app = express();
+//passport middleware
+//app.use(passport.initialize());
+//passport config
+//  
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+const port = 5000;
+app.use("/", Request);
+app.get("/sample", (req, res) => {
+    res.send("Hello World");
+});
+app.listen(port, () => {
+    console.log(`listning to port ${port}`);
+});
+//# sourceMappingURL=server.js.map
