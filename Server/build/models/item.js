c51692a5bd37461368d91a9620722b5f09e005ff
okazakikirito <okazakikirito@gmail.com>
2018-09-16 14:55:24 +0530

    initial

--- Server/build/models/item.js
+++ Server/build/models/item.js
@@ -5,10 +5,6 @@ const itemSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    },
-    quantity: {
-        type: Number,
-        required: true
     }
 });
 module.exports = mongoose.model("Item", itemSchema);
