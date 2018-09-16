cc5bb145d1d1bc3c2553a3249cc3c028fee0c0ce
Supun Dileepa <dilee1st.1995@gmail.com>
2018-09-17 00:02:03 +0530

    items route updated

--- Server/routes/api/items.js
+++ Server/routes/api/items.js
@@ -16,12 +16,31 @@ router.get("/", (req, res, next) => {
 });
 
 
+//ITEM FIND BY ID
+
+router.get("/:id", (req, res, next) => {
+    try {
+        Item.findOne({
+            _id: req.params.id
+        }, (err, result) => {
+            if (err) return next(err);
+            res.json(result);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
+
+
 //ADD ITEMS
 router.post("/", (req, res, next) => {
     try {
         const item = new Item({
             id: req.body.id,
-            name: req.body.name
+            name: req.body.name,
+            unit: req.body.unit
         });
         item.save((err, result) => {
             if (err) {
@@ -41,7 +60,29 @@ router.post("/", (req, res, next) => {
 });
 
 //Update Item
-
+router.put('/:id', (req, res, next) => {
+    Item.findOneAndUpdate({
+            _id: req.params.id
+        }, {
+            $set: {
+                name: req.body.name,
+                unit: req.body.unit
+            }
+        },
+        (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    title: "An error occured",
+                    error: err
+                });
+            }
+            res.status(201).json({
+                message: "Item Updated",
+                result
+            });
+        }
+    )
+})
 
 
 //REMOVE ITEMS
@@ -50,7 +91,10 @@ router.delete('/:id', (req, res, next) => {
         _id: req.params.id
     }, (err, item) => {
         if (err) return res.json(err);
-        res.json(item);
+        res.json({
+            msg: "item deleted",
+            item: item
+        });
     });
 });
 
