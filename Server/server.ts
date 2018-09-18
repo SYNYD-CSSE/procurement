31b61bd5678c61f5ca370b9f9d1794c8b69b6aee
Samith Dilshan <samithdilsh@gmail.com>
2018-09-18 17:06:02 +0530

    html update

--- Server/server.ts
+++ Server/server.ts
@@ -50,7 +50,7 @@ app.use("/employees",employeeRoutes);
 generate.initilize();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + './../public/index.html'));
 });
 
 
