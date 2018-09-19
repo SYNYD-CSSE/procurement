59e407122a9128b41cb7f94118f5a4e49c4c8f54
Bimali-YMY <b.yapabmmv@gmail.com>
2018-09-19 22:02:38 +0530

    commit working tree

--- Server/server.ts
+++ Server/server.ts
@@ -1,17 +1,24 @@
+
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import passport from "passport";
 
-const path = require("path");
-//const passport = require("passport");
-const config = require("./config/database");
-const Order = require("./routes/api/orders");
-const Item = require("./routes/api/items");
-const Supplier = require("./routes/api/suppliers");
-const InventroyItems = require("./routes/api/inventoryItems");
+import generate from "./config/generate";
 
-mongoose.connect(config.database);
+import path from "path";
+import config from "./config/database";
+
+import employeeRoutes from "./routes/EmployeeRoutes";
+import Order from "./routes/api/orders";
+import Item from "./routes/api/items";
+import Payment from "./routes/api/paymentRoute";
+import Supplier from "./routes/api/suppliers";
+import InventroyItems from "./routes/api/inventoryItems";
+
+
+mongoose.connect(config.database,{useNewUrlParser: true});
 
 mongoose.connection.on("connected", () => {
   console.log(`connected to database ${config.database}`);
@@ -38,14 +45,18 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = 5000;
-
 app.use("/items", Item);
 app.use("/orders", Order);
+app.use("/payment", Payment);
+app.use("/employees",employeeRoutes);
 app.use("/suppliers", Supplier);
 app.use("/invetoryItems",InventroyItems);
-app.get("/sample", (req, res) => {
-  res.send("Hello World");
+generate.initilize();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + './../public/index.html'));
 });
+
 
 app.listen(port, () => {
   console.log(`listning to port ${port}`);
