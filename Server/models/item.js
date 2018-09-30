68e998c685504278d6739876882a9bc60f40315e
Supun Dileepa <dilee1st.1995@gmail.com>
2018-09-30 12:39:03 +0530

    remove qty from item model

--- Server/models/item.js
+++ Server/models/item.js
@@ -13,11 +13,6 @@ const itemSchema = new mongoose.Schema({
         required: true
     },
 
-    quantity: {
-        type: Number,
-        default: 1,
-        min: 1
-    },
     unit: {
         type: String
     }
