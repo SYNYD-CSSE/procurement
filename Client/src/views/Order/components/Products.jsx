f6ddb6dde2a4422b03029fd97caa5ab56cbd5beb
Samith Dilshan <samithdilsh@gmail.com>
2018-10-03 07:19:06 +0530

    fix library error of Order

--- Client/src/views/Order/components/Products.jsx
+++ Client/src/views/Order/components/Products.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import LoadingProducts from "../loaders/Products";
 import NoResults from "../empty-states/NoResults";
-import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
+import CSSTransitionGroup from "react-transition-group/CSSTransition";
 
 class Products extends Component {
   constructor() {
