8cb45493060effda54527915de41316bece5f5dd
kirito <okazakikirito@gmail.com>
2018-10-04 22:11:10 +0530

    icons

--- Client/src/views/Pages/Changes/ChangeItems.js
+++ Client/src/views/Pages/Changes/ChangeItems.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import { Badge, Col, Nav, NavItem, NavLink, Row, TabContent, TabPane ,Card ,Table , CardBody , CardHeader} from 'reactstrap';
 import { Link } from "react-router-dom";
-
+//import edit from '@material-ui/icons/edit';
+//import delete_forever from '@material-ui/icons/delete_forever';
 class ChangeItems extends Component {
 
 constructor(props){
@@ -29,7 +30,10 @@ constructor(props){
                     <td>{this.state.item.itemId}</td>
                     <td>{this.state.item.name}</td>
                     <td>{this.state.item.unit}</td>
-                    <td>{this.state.item.quantity}</td>     
+                    <td>{this.state.item.quantity}</td>  
+                    <td>  <i className="cui-pencil icons font-2xl d-block mt-4" onClick={()=>handleEdit()}></i></td>
+                    <td> <i className="cui-trash icons font-2xl d-block mt-4" onClick={()=>handleDelete()}></i></td>  
+               
         </tr>
     )
     
@@ -38,3 +42,5 @@ constructor(props){
 }
 
 export default ChangeItems;
+
+
